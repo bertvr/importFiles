321bf271c0db3504eb257fc1aef81a16d9a3e95c
unknown <103023@PCC7-201202005.ecorp.int>
2017-04-07 20:47:04 +0200

    json file

--- importfile.py
+++ importfile.py
@@ -1,23 +1,8 @@
-# from "file" import "class"
+
+import json
+import jsonpickle
 from Employee import Employee
 from Employee import Boss
-
-
-print ("Hello, Python!")
-
-print ("Hello, Python! on github")
-
-
-
-#print dictionarys
-dict = {'Name': 'Zara', 'Age': 20, 'Class': 'First'}
-for emp in dict:
-    print(emp + " => " +  str(dict[emp]))
-
-
-#range for for-loops
-for i in range(1,12,1):
-    print ( "range: " + str(i))
 
 
 #def object lists
@@ -27,19 +12,30 @@ employees.append(Employee("Wouter",5000))
 employees.append(Employee("stijn", 1000))
 employees.append( Boss("Bert", 1000))
 
-print(employees)
-
-employees[0].displayEmployee()
-employees[1].displayEmployee()
-employees[2].displayEmployee()
-employees[1].displayEmployeeHiddenAtribute()
-employees[1].displayCount()
 
 
-print ("Employee.__doc__:", Employee.__doc__)
-print ("Employee.__name__:", Employee.__name__)
-print ("Employee.__module__:", Employee.__module__)
-print ("Employee.__bases__:", Employee.__bases__)
-print ("Employee.__dict__:", Employee.__dict__ )
+# s = json.dumps(employees.__dict__,default=Employee)
+# print(s)
+
+serialized = jsonpickle.encode(employees)
+print(serialized)
 
 
+# Open a file
+exportFile = open("export.txt", "w")
+exportFile.write( serialized)
+exportFile.close()
+
+
+
+# f  = FileItem("/foo/bar")
+# magic(f)
+#
+# print ("Hello, Python!")
+#
+# class MyEncoder(JSONEncoder):
+#     def default(self, o):
+#         return o.__dict__
+#
+# MyEncoder().encode(f)
+#     '{"fname": "/foo/bar"}'
